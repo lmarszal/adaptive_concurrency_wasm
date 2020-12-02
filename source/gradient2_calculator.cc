01f6b0cd84e4f7b510162c5bc9967533a9b073c9
Łukasz Marszał <lmarszal@gmail.com>
2020-12-02 15:04:23 +0100

    gradient2_calculator tests

--- source/gradient2_calculator.cc
+++ source/gradient2_calculator.cc
@@ -1,9 +1,13 @@
 #include "gradient2_calculator.hpp"
 
 #include <algorithm>
+#include <cmath>
 
 uint32_t Gradient2Calculator::calculateLimit(double shortRtt, double longRtt, uint32_t limit, uint32_t inflight)
 {
+    // allow some queueing
+    auto queueSize = std::max(std::sqrt(limit), 4.0);
+
     // Don't grow the limit if we are app limited
     if (inflight < limit / 2) {
         return limit;
@@ -14,7 +18,7 @@ uint32_t Gradient2Calculator::calculateLimit(double shortRtt, double longRtt, ui
     // allow it to be reduced by more than half to avoid aggressive load-shedding due to 
     // outliers.
     auto gradient = std::clamp(tolerance * longRtt / shortRtt, 0.5, 1.0);
-    auto newLimit = limit * gradient + limit;
+    auto newLimit = gradient * limit + queueSize;
     newLimit = limit * (1 - smoothing) + newLimit * smoothing;
     uint32_t newLimit_ui32 = std::clamp((uint32_t)newLimit, min_limit, max_limit);
 
